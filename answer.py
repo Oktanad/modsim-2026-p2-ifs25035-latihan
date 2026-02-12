7a2fbe83599d8218c5e68e177bfc072c2eabf7ff
Oktanad <oktanad24@gmail.com>
2026-02-12 10:21:27 +0700

    commit

--- answer.py
+++ answer.py
@@ -1,49 +1,40 @@
-# Read input
-q = input()
+target_question = input()
 
-# Prepare output variable
-out = ""
+if target_question == "q1":
+    print("S|1176|61.2")
 
-if q == "q1":
-    out = "S|1176|61.2"
+elif target_question == "q2":
+    print("STS|4|0.2")
 
-elif q == "q2":
-    out = "STS|4|0.2"
+elif target_question == "q3":
+    print("Q9|21|18.6")
 
-elif q == "q3":
-    out = "Q9|21|18.6"
+elif target_question == "q4":
+    print("Q16|75|66.4")
 
-elif q == "q4":
-    out = "Q16|75|66.4"
+elif target_question == "q5":
+    print("Q2|36|31.9")
 
-elif q == "q5":
-    out = "Q2|36|31.9"
+elif target_question == "q6":
+    print("Q9|8|7.1")
 
-elif q == "q6":
-    out = "Q9|8|7.1"
+elif target_question == "q7":
+    print("Q12|3|2.7")
 
-elif q == "q7":
-    out = "Q12|3|2.7"
+elif target_question == "q8":
+    print("Q12|3|2.7")
 
-elif q == "q8":
-    out = "Q12|3|2.7"
+elif target_question == "q9":
+    print("Q1:0.9|Q2:0.9|Q9:0.9|Q11:0.9")
 
-elif q == "q9":
-    out = "Q1:0.9|Q2:0.9|Q9:0.9|Q11:0.9"
+elif target_question == "q10":
+    print("4.80")
 
-elif q == "q10":
-    out = "4.80"
+elif target_question == "q11":
+    print("Q5:4.95")
 
-elif q == "q11":
-    out = "Q5:4.95"
+elif target_question == "q12":
+    print("Q12:4.59")
 
-elif q == "q12":
-    out = "Q12:4.59"
-
-elif q == "q13":
-    out = "positif=1396:72.7|netral=471:24.5|negatif=54:2.8"
-
-else:
-    out = ""
-
-print(out)
+elif target_question == "q13":
+    print("positif=1396:72.7|netral=471:24.5|negatif=54:2.8")
